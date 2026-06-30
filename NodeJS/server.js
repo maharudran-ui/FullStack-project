35c3fad3a7362b6678271058961c7633e0c71cfc
MAHARUDRA N. (DOLLARBIRD) <DBI041@DOLLARBIRD.MYS>
2026-06-30 22:59:29 +0530

    Changed server.js

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -23,10 +23,8 @@ app.use("/api/orders", orderRoutes);
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-
-  
 });
