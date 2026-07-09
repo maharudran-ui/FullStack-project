ed65ef03a14c663e4ad2325902440fd71bb71232
MAHARUDRA N. (DOLLARBIRD) <DBI041@DOLLARBIRD.MYS>
2026-07-09 17:00:21 +0530

    testing category by admin

--- react-BootStrap-webpage/src/pages/TrackOrder.jsx
+++ react-BootStrap-webpage/src/pages/TrackOrder.jsx
@@ -44,6 +44,7 @@ function TrackOrder() {
     "Maps, Prints, Photographs Ephemera": 4,
     "Old Picture Postcards": 5,
     "Stamps and Covers": 6,
+    "History":27,
   };
 
   const getCategoryId = (name) => categoryMap[name];
