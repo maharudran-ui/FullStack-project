08f069b002234a149990b77bbf26ff9fc53191d2
MAHARUDRA N. (DOLLARBIRD) <DBI041@DOLLARBIRD.MYS>
2026-07-10 15:50:55 +0530

    fixed errors

--- react-BootStrap-webpage/src/pages/TrackOrder.jsx
+++ react-BootStrap-webpage/src/pages/TrackOrder.jsx
@@ -31,29 +31,23 @@ function TrackOrder() {
   const [searchDescription, setSearchDescription] = useState(false);
   const [hideCategoryHighlight, setHideCategoryHighlight] = useState(false);
 
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    loadCategories();
-}, []);
-
-const loadCategories = async () => {
-    const res = await api.get("/categories/get");
-    setCategories(res.data);
-};
-
   useEffect(() => {
     setCurrentPage(1);
   }, [searchKeyword, searchScope, searchDescription]);
 
   const location = useLocation();
-const getCategoryId = (name) => {
-    const category = categories.find(
-        c => c.category_name === name
-    );
 
-    return category?.category_id;
-};
+  const categoryMap = {
+    "Philatelic Books": 1,
+    "West Indies Books": 2,
+    "Miscellaneous books": 3,
+    "Maps, Prints, Photographs Ephemera": 4,
+    "Old Picture Postcards": 5,
+    "Stamps and Covers": 6,
+    "History":25,
+  };
+
+  const getCategoryId = (name) => categoryMap[name];
 
   // fetchproducts
 
@@ -204,7 +198,7 @@ const getCategoryId = (name) => {
   );
   const totalPages = Math.ceil(currentProducts.length / itemsPerPage);
 
-  // const isProductCategory = Object.keys(categoryMap).includes(activeCategory);
+  const isProductCategory = Object.keys(categoryMap).includes(activeCategory);
 
   const handleVerify = async () => {
     try {
